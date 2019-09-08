d76c3a125b1222cd791a31e0c0a992d5efef02ea
Red Cap <idredcap@gmail.com>
2019-09-08 16:17:21 +0300

    190908. [HomeWork:0] Internet-Shop.

---
+++ 0_ishop/InternetShop.js
@@ -0,0 +1,41 @@
+﻿var InternetShop = React.createClass({
+
+  displayName: 'InternetShop',
+
+  getDefaultProps: function() {
+    return { question: "Вопрос ни о чём" }
+  },
+
+  render: function() {
+
+    let items = [];
+    this.props.shopItems.forEach(item  => {
+      const itemHash = 
+        React.DOM.tr({ key: item.id, className: 'shop__item' },
+          React.DOM.td({ className: 'shop__item-name' }, item.name),
+          React.DOM.td({ className: 'shop__item-price' }, item.price),
+          React.DOM.td({ className: 'shop__item-image' },
+            React.DOM.img({ src: item.image })
+          ),
+          React.DOM.td({ className: 'shop__item-stock' }, item.stock),
+      );
+
+      items.push(itemHash);
+    });
+
+    let headers = [];
+    this.props.shopItemsHeader.forEach((header, index) => {
+      const headerHash = React.DOM.th({ key: index, className: 'shop__item-header' }, header);
+      headers.push(headerHash);
+    });
+
+    return React.DOM.div({ className:'shop' }, 
+      React.DOM.h1({ className: 'shop__name' }, this.props.shopName),
+      React.DOM.table({ className: 'shop__table' },
+        React.DOM.tr({ className: 'shop__header' }, headers),
+        items,
+      ),
+    );
+  },
+
+});
---
+++ 0_ishop/InternetShopService.js
@@ -0,0 +1,33 @@
+getItems = function () {
+    return [
+        {
+            id: 1736,
+            name: 'Acer Aspire XC-885 (DT.BAQER.027)',
+            price: 450,
+            image: "https://static.acer.com/up/Resource/Acer/Desktop/Aspire%20XC/Photogallery/20180530/Aspire-XC-885-photogallery-03.png",
+            stock: 73
+        }, {
+            id: 7829,
+            name: 'Intel NUC Hades Canyon (NUC8I7HVK)',
+            price: 1025,
+            image: "https://www.intel.ru/content/dam/products/hero/foreground/nuc8i7hvk-nuc8i7hnk-extreme-angle1-skull-16x9.png.rendition.intel.web.978.550.png",
+            stock: 4
+        }, {
+            id: 4203,
+            name: 'MacBook Pro',
+            price: 3000,
+            image: "https://static.re-store.ru/upload/resize_cache/iblock/4fc/1120_772_17f5c944b3b71591cc9304fac25365de2/4fc2f36b75ade15f78baeefb04209361.jpg",
+            stock: 14
+        }, {
+            id: 9207,
+            name: 'Raspberry Pi 4',
+            price: 35,
+            image: "https://www.raspberrypi.org/app/uploads/2019/06/HERO-ALT-300x214.jpg",
+            stock: 184
+        }
+    ]
+}
+
+getItemsHeader = function () {
+    return ['Наименование', 'Цена', 'Изображение', 'Остаток']    
+}
